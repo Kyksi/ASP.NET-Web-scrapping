e362de0bf45f3eee5592f6dbef7c41ff421957b6
Kyksi <47677914+Kyksi@users.noreply.github.com>
2019-02-15 23:29:42 +0100

    Add files via upload

---
+++ web-scraping.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="web-scraping.aspx.cs" Inherits="ASP.NET_pages.web_scraping" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=0.65"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"/>
+    <style>
+        body{
+            background: url(https://i.pinimg.com/originals/a2/04/93/a20493184839815c05371cee41543e28.jpg) no-repeat fixed; /* Добавляем фон */
+            background-size: cover; /* Масштабируем фон */
+        }
+        .serial{
+            margin-bottom: 35px;
+
+            display: inline-block;
+            margin-left: 20px;
+	        margin-right: 20px;
+	        background: rgba(255,255, 255, 0.75) none repeat scroll 0 0;
+	        border: 1px solid #c0c0c0;
+	        height: auto;
+	        width: auto;
+	        overflow: hidden;
+	        padding: 25px 15px;
+	        position: relative;
+	        transition: all 0.5s ease 0s;
+        }
+        .serial:hover {
+	        box-shadow: 0 0 16px rgba(0, 0, 0, 0.5);
+        }
+        p {
+            padding: 15px 15px 15px 15px;
+        	cursor: pointer;
+        	text-transform: uppercase;
+            max-width: 200px;
+            text-align: center;
+        }
+        img{
+            height: auto;
+	        max-width: 100%;
+        }
+        .row1{
+            display:flex;
+            flex-wrap:wrap;
+            align-items:stretch;
+            justify-content: center;
+            margin-top: 25px;
+        }
+    </style>
+    <title>Web-scraping</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <a style="margin-left: 10px;" href="https://vod.pl/seriale">Link do strony</a>
+        <div class="seriales">
+            <asp:Panel ID="Panel1" runat="server"></asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
---
+++ web-scraping.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+
+namespace ASP.NET_pages
+{
+    public partial class web_scraping : System.Web.UI.Page
+    {
+        List<string> ImgList = new List<string>();
+        List<string> TitleList = new List<string>();
+        List<string> UrlList = new List<string>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            step1();
+            getholidays_2018();
+        }
+
+        public void step1()
+        {
+            string url = "https://vod.pl/seriale";
+
+            HtmlDocument document = new HtmlDocument();
+            StreamReader reader = new StreamReader(WebRequest.Create(url).GetResponse().GetResponseStream(), Encoding.Default);
+            document.Load(reader);
+
+            HtmlNode someNode = document.GetElementbyId("SiteSerialeTop");
+
+            HtmlNodeCollection Link = someNode.SelectNodes("ul/li/span/a");
+            HtmlNodeCollection Img = someNode.SelectNodes("ul/li/span/a/img");
+
+
+            foreach (HtmlNode n in Link)
+            {
+                HtmlAttribute att = n.Attributes["href"];
+                string title = n.InnerText;
+                string newurl = att.Value;
+                UrlList.Add(newurl);
+                TitleList.Add(title);
+            }
+
+            foreach (HtmlNode n in Img)
+            {
+                HtmlAttribute att = n.Attributes["data-original"];
+                string src = att.Value;
+                ImgList.Add(src);
+            }
+
+        }
+
+        private void getholidays_2018()
+        {
+            int i = 0;
+            Panel1.Controls.Add(new LiteralControl("<div class='row1'>"));
+            foreach (string u in ImgList)
+            {
+                Panel1.Controls.Add(new LiteralControl("<div class='serial'>" +
+                    "<a title='" + TitleList[i] + "' href=" + UrlList[i] + "> <img src= " + u +"></a>" +
+                    "<p>" + TitleList[i] + "</p> </div>"));
+                i++;
+            }
+            Panel1.Controls.Add(new LiteralControl("</div>"));
+        }
+    }
+}
